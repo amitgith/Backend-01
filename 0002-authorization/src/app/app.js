dfbd86fc2c8e4487501646faabb482c700e10889
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-03 15:33:56 +0530

    feat: connect MongoDB and add password middleware

--- 0002-authorization/src/app/app.js
+++ 0002-authorization/src/app/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import bycrptjs from "bcryptjs";
 dotenv.config();
 import jwt from "jsonwebtoken";
+import userModel from "../models/user.model.js";
+import { authenticate } from "../middleware/auth.middleware.js";
 const app = express();
 // middleware
 app.use(express.json());
@@ -10,14 +13,16 @@ app.get("/api", (req, res) => {
     message: "Welcome to authentication",
   });
 });
-app.post("/api/auth/register", (req, res) => {
+app.post("/api/auth/register", async (req, res) => {
   const { name, email, password } = req.body;
-  //   save data to db
+  const user = await userModel.create({
+    name,
+    email,
+    password: await bycrptjs.hash(password, 10),
+  });
   const token = jwt.sign(
     {
-      name,
-      email,
-      // _id
+      id: user._id,
     },
     process.env.JWT_SECRET,
   );
@@ -27,8 +32,19 @@ app.post("/api/auth/register", (req, res) => {
       user: {
         name,
         email,
+        id: user._id,
       },
       token,
+    },
+  });
+});
+
+app.get("/api/auth/me", authenticate, async (req, res) => {
+  console.log(req.user);
+
+  res.status(200).json({
+    data: {
+      user: req.user,
     },
   });
 });
--- 0002-authorization/src/server.js
+++ 0002-authorization/src/server.js
@@ -1,4 +1,6 @@
 import app from "./app/app.js";
+import { connectdb } from "./config/db.js";
+await connectdb();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
