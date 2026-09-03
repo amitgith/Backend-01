545cabe5c925c23b667f9bb1942cf617f57ca8bc
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-03 16:03:19 +0530

    completed done

--- 0002-authorization/src/app/app.js
+++ 0002-authorization/src/app/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import bycrptjs from "bcryptjs";
+import bycrpt from "bcryptjs";
 dotenv.config();
 import jwt from "jsonwebtoken";
 import userModel from "../models/user.model.js";
@@ -45,6 +46,33 @@ app.get("/api/auth/me", authenticate, async (req, res) => {
   res.status(200).json({
     data: {
       user: req.user,
+    },
+  });
+});
+app.post("/api/auth/login", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await userModel.findOne({
+    email,
+  });
+  const isValidPassword = bycrpt.compare(password, user.password);
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Invalid email or password",
+    });
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+  res.status(200).json({
+    message: "User LoggedIn Successfully",
+    data: {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
     },
   });
 });
