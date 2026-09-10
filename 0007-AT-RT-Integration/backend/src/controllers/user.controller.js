ce67c71f6ed6774d3ba6f5eb4b5223c17ed67686
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-09-10 10:58:42 +0530

    Access token refreshed successfully

--- 0007-AT-RT-Integration/backend/src/controllers/user.controller.js
+++ 0007-AT-RT-Integration/backend/src/controllers/user.controller.js
@@ -1,3 +1,11 @@
+import userModel from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import {
+  generateTokens,
+  verifyAccessToken,
+  verifyRefreshToken,
+} from "../utils/auth.js";
+
 export const apiController = (req, res) => {
   try {
     console.log("Ok got it");
@@ -8,6 +16,110 @@ export const apiController = (req, res) => {
     console.log(error.message);
     res.status(400).json({
       message: "Authentication connection error",
+    });
+  }
+};
+
+export const registerApiController = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const isUserExists = await userModel.findOne({ email });
+    if (isUserExists) {
+      return res.status(401).json({
+        message: "User already exists",
+        errors: {
+          path: "email",
+          message: "User already exists",
+        },
+      });
+    }
+    const user = await userModel.create({
+      name,
+      email,
+      passwordHash: await bcrypt.hash(password, 10),
+    });
+    const { accessToken, refreshToken } = generateTokens({ userId: user._id });
+    user.refreshToken = refreshToken;
+    await user.save();
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+    });
+    res.status(201).json({
+      message: "User registered successfully",
+      data: {
+        user: {
+          name: user.name,
+          email: user.email,
+        },
+      },
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Interval server server",
+    });
+  }
+};
+export const aboutMeApiController = async (req, res) => {
+  const accessToken = req.headers.authorization?.split(" ")[1];
+  if (!accessToken) {
+    return res.status(401).json({
+      message: "Unauthorized, access token not found",
+    });
+  }
+  try {
+    const decoded = verifyAccessToken(accessToken);
+    const user = await userModel.findById(decoded.id);
+    res.status(200).json({
+      message: "user fetched successfully",
+      data: {
+        user: {
+          name: user.name,
+          email: user.email,
+        },
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(401).json({
+      message: "Unathorized, Invalid or expired access token",
+    });
+  }
+};
+export const refreshApiController = async (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) {
+    return res.status(401).json({
+      message: "Unathorized, refresh token not found",
+    });
+  }
+  try {
+    const decoded = verifyRefreshToken(refreshToken);
+    const user = await userModel.findById(decoded.id);
+    if (refreshToken !== user.refreshToken) {
+      user.refreshToken = null;
+      await user.save();
+      return res.status(401).json({
+        message: "Unathorized, refresh token mismatch",
+      });
+    }
+    const { accessToken, refreshToken: newRefreshToken } = generateTokens({
+      userId: user._id,
+    });
+    res.cookie("refreshToken", newRefreshToken, {
+      httpOnly: true,
+    });
+    user.refreshToken = newRefreshToken;
+    await user.save();
+    return res.status(200).json({
+      message: "Access token refreshed successfully",
+      accessToken,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({
+      message: "Interval Server error",
     });
   }
 };
--- 0007-AT-RT-Integration/backend/src/routes/auth.routes.js
+++ 0007-AT-RT-Integration/backend/src/routes/auth.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { apiController } from "../controllers/user.controller.js";
+import {
+  aboutMeApiController,
+  apiController,
+  refreshApiController,
+  registerApiController,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 router.get("/api", apiController);
+router.post("/register", registerApiController);
+router.get("/me", aboutMeApiController);
+router.post("/refresh", refreshApiController);
 
 export default router;
